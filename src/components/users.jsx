88f84cf60370cc32b170dffef26ec4aff413bd98
Aleksey <alexblackskja@gmail.com>
2022-07-11 21:21:28 +0300

    version 2 kp1

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -4,7 +4,7 @@ import api from "../api"
 const Users = () => {
     const [users, setUsers] = useState(api.users.fetchAll())
     const handleDelete = (userId) => {
-        setUsers((prevState) => prevState.filter((user) => user._id !== userId));
+        setUsers((prevState) => prevState.filter(({_id}) => _id !== userId));
     }
 
     const renderPhrase = (number) => {
@@ -14,11 +14,7 @@ const Users = () => {
         return `${number} человек тусанет`;
     }
 
-    const getBageClasses = (color) => {
-        let classes = "badge m-2 bg-";
-        classes += color;
-        return classes
-    }
+    const getBadgeClasses = (color) => `badge m-2 bg-${color}`;
 
     if (users.length === 0) {
         return <h1><span className="badge bg-danger"> Никто с тобой не тусанет</span></h1>
@@ -35,23 +31,31 @@ const Users = () => {
                     <th scope="col">Проффессия</th>
                     <th scope="col">Встретился, раз</th>
                     <th scope="col">Оценка</th>
-                    <th scope="col"></th>
+                    <th scope="col"/>
                     </tr>
                 </thead>
-                <tbody>{users.map((user) => (
-                        <tr key={user._id}>
-                            <td>{user.name}</td>
+                <tbody>{users.map((
+                        {
+                         _id,
+                         name,
+                         qualities,
+                         profession,
+                         completedMeetings,
+                         rate           
+                        }) => (
+                        <tr key={_id}>
+                            <td>{name}</td>
                             <td>
-                                {user.qualities.map((quality) => (
-                                    <span key={quality._id} className={getBageClasses(quality.color)}>{quality.name}</span>
+                                {qualities.map(({_id, color, name}) => (
+                                    <span key={_id} className={getBadgeClasses(color)}>{name}</span>
                                     ))
                                 }
                             </td>
-                            <td>{user.profession.name}</td>
-                            <td>{user.completedMeetings}</td>
-                            <td>{user.rate}/5</td>
+                            <td>{profession.name}</td>
+                            <td>{completedMeetings}</td>
+                            <td>{rate}/5</td>
                             <td>
-                                <button className="btn bg-danger btn-sm m-2" onClick={() => handleDelete(user._id)}>delete</button>
+                                <button className="btn bg-danger btn-sm m-2" onClick={() => handleDelete(_id)}>delete</button>
                             </td>
                         </tr>
                     ))
