8b337d09ea1641ff5d8645a81d8624516a1088fa
Aleksey <alexblackskja@gmail.com>
2022-08-23 06:20:08 +0300

    form 1

--- src/app/components/login.jsx
+++ src/app/components/login.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-    return <h1>Login</h1>;
+    const [data, setData] = useState({ email: "", password: "" });
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: target.value
+        }));
+    };
+    return (
+        <form action="">
+            <div>
+                <label htmlFor="email">Email</label>
+                <input
+                    type="text"
+                    id="email"
+                    value={data.email}
+                    onChange={handleChange}
+                    name="email"
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Пароль</label>
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={data.password}
+                    onChange={handleChange}
+                />
+            </div>
+        </form>
+    );
 };
 
 export default Login;
