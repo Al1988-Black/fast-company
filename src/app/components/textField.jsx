72b8b0466acf1bac34c7816b8981652abb0cb2b1
Aleksey <alexblackskja@gmail.com>
2022-08-25 06:02:51 +0300

    form3

--- src/app/components/textField.jsx
+++ src/app/components/textField.jsx
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const TextField = ({ label, type, name, value, onChange }) => {
+const TextField = ({ label, type, name, value, onChange, error }) => {
+    const [showPassword, setShowPassord] = useState(false);
+    const getInputClasses = () => {
+        return "form-control" + (error ? " is-invalid" : "");
+    };
+    const toggleShowPassword = () => {
+        setShowPassord((prevState) => !prevState);
+    };
     return (
-        <div>
+        <div className="mb-4">
             <label htmlFor={name}>{label}</label>
-            <input
-                type={type}
-                id={name}
-                value={value}
-                onChange={onChange}
-                name={name}
-            />
+            <div className="input-group has-validation">
+                <input
+                    type={showPassword ? "text" : type}
+                    id={name}
+                    value={value}
+                    onChange={onChange}
+                    name={name}
+                    className={getInputClasses()}
+                />
+                {type === "password" && (
+                    <button
+                        className="btn btn-outline-secondary"
+                        type="button"
+                        onClick={toggleShowPassword}
+                    >
+                        <i
+                            className={
+                                "bi bi-eye" + (showPassword ? "-slash" : "")
+                            }
+                        ></i>
+                    </button>
+                )}
+                {error && <div className="invalid-feedback">{error}</div>}
+            </div>
         </div>
     );
 };
@@ -23,7 +47,8 @@ TextField.propTypes = {
     type: PropTypes.string,
     name: PropTypes.string,
     value: PropTypes.string,
-    onChange: PropTypes.func
+    onChange: PropTypes.func,
+    error: PropTypes.string
 };
 
 export default TextField;
